7e67dc029d4b97bd8b9dbc6f7e9077ed1a024b2f
Kachi Was Here <35039093+Kachjs@users.noreply.github.com>
2022-01-31 08:24:31 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,40 @@
+module.exports = {
+    bot: {
+        botOwner: ["717709392119857203"],//Bot Sahibi İdiniz
+        botToken: "OTM3MjY5MzUyNzc5.bismillahyalahvermemtokenfelan",//Botun Tokenı 
+        botPrefix: ".",//Botun Prefixi
+        botStatus: "Kachi ❤️ Rose",//Bot Durumu Kafanıza Göre Girin
+        mongoURL: "",//Mongo Url Gireceksiniz Buraya
+    },
+    roles: {
+        vipStaff: ["937013974237601883"],//Vip Hammer İd
+        registerStaff: ["937013974237601883"],//Register Hammer İd
+        manRoles: ["937013993061625917", "937013994567393360"], // erkek rolleri
+        womanRoles: ["937013991404875828", "937013992453447731"], // kız rolleri
+        unregisterRoles: ["937013995615965184"],//Kayıtsız İd
+        tagRole: "937013980243849226",//Tag Rol İd
+        vipRole: "937013984232624230",//Vip Rol İd
+        boosterRole: "934777097812054027",//Booster Rol İd
+        suspecious: "937014010606419998" // şüpheli hesap rolü
+    },
+    channels: {
+        registerChannel: "937014037827448893",//Hosgeldin Kanalı İd
+        rulesChannel: "937014043825287208",//Kurallar Kanalı İd
+        botVoice: "937014040402722859",// Botun Sese Girecek Kanal İd
+
+    },
+    guild: {
+        guildID: "934514804494831717",//Sunuucu İd
+        tag: "", // İsim Başına Tag Gelsin Diyorsanız Buraya Tagı Giriniz
+        tagges: ["", "", "", "", ""], // Birden Fazla Tagınız Varsa Girin Örnek Olarak #0001 Gibi
+        defaultTag: "",
+        defaultName: "Rose İsim | Yaş",
+        suspeciousName: "Rose Şüpheli Hesap",
+    },
+    emojis: {
+        yes: "<a:evet:937271435859202088>",
+        no: "<a:hayr:937271437394329651>",
+        // Emoji İdlerini Girin
+    }
+
+};
